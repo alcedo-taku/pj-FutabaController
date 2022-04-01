896223adbb9c7b5fe29b801684272b467f8e1a7f
立脇 拓 <takuji.coroton.thanks@gmail.com>
2022-04-01 17:24:04 +0900

    docs: 送信用構造体にコメントを追加

--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -63,13 +63,13 @@ uint8_t vrOffset[5];
 
 //communication data 通信データ
 struct DataControllerToMain{
-	uint8_t trm;
-	uint8_t sw1;
-	uint8_t sw2;
-	uint8_t btn;
-	uint8_t edt;
-	int8_t stk[4]; //RX, RY, LX, LY
-	uint8_t vr[5];
+	uint8_t trm; // RX-p, RX-n, RY-n, RY-p, LY-n, LY-p, LX-p, Lx-n
+	uint8_t sw1; // A, B, C↓, C↑, D
+	uint8_t sw2; // E↓, E↑, F, G, H
+	uint8_t btn; // 未実装
+	uint8_t edt; // Mode/Page, End, Select↑, Select↓
+	int8_t stk[4]; // RX, RY, LX, LY
+	uint8_t vr[5]; // VR(A), VR(B), VR(C), Left, Right
 };
 DataControllerToMain data1;
 
